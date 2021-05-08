3fa67c44a60e607f69cb3e79facfc91f99d7fbc0
Adrian Biedrzycki <a@modules.io>
2021-05-08 10:40:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import {Box} from "box.js"
+import {Box} from "./box.js"
 
 const render = () => {
   return (
