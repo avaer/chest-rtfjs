219ee640af3de4ccfd5b0ea2879bfa8ba24b4e06
Avaer Kazmer <a@modules.io>
2021-05-08 17:19:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-import {React, Fragment} from 'react-all';
+import {React, Fragment, Suspense, GLTFLoader, useLoader} from 'react-all';
 import {Box} from "./box.js"
+
+const Chest = () => (
+  <Gltf src="http://127.0.0.1:10000/chest-rtfjs/chest.glb"/>
+);
+function Gltf({src}) {
+  const gltf = useLoader(GLTFLoader, src);
+  return <primitive object={gltf.scene} position={[0, 0, 0]} />
+}
 
 const render = () => {
   return (
@@ -8,6 +16,9 @@ const render = () => {
       <pointLight position={[10, 10, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <Suspense fallback={<Box position={[0, 0, 0]} />}>
+        <Chest />
+      </Suspense>
     </Fragment>
   );
 };
